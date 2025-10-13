dd61fdda052d6b82f5f781b6f1d93eafd2a1e990
HaVanTinhTTI <phongtran.hvt@gmail.com>
2025-10-13 21:49:55 +0700

    Create supabase.js

---
+++ public/supabase.js
@@ -0,0 +1,5 @@
+// supabase.js
+const client = supabase.createClient(
+  'https://chjqassbtcqthueeddou.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNoanFhc3NidGNxdGh1ZWVkZG91Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAxMzY2OTgsImV4cCI6MjA3NTcxMjY5OH0.FbWGSw9sqUkb2_ppErN4QTFGy1Cho-HX4XbR5_-KB0I'
+);
